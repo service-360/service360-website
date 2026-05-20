5f8e12cadd823e5c3e75fa48adf53b4ed14b31ce
service-360 <admin@service360.in>
2026-05-20 17:59:52 +0530

    Add partner with us page

--- app/page.js
+++ app/page.js
@@ -705,6 +705,9 @@ ${message}
 
           {/* Company */}
           <div>
+            <a href="/partner-with-us" className="hover:text-white transition">
+  Partner With Us
+</a>
 
             <h3 className="text-2xl font-semibold text-white mb-6">
               Company
