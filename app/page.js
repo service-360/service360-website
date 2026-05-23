5c79fb0fa0eb6237df9e12a8a1bd440f4ebc82bb
service-360 <admin@service360.in>
2026-05-23 21:41:17 +0530

    Fix homepage imports and reusable layout

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client"
 
 import { useState } from "react"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function Home() {
 
@@ -358,7 +358,7 @@ ${message}
 
       </section>
 
-      {/* WhatsApp */}
+      {/* WhatsApp Floating Button */}
       <a
         href="https://wa.me/916369051521"
         target="_blank"
