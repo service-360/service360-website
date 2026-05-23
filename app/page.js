355cc7782e9bb5c7630b0fc27d1c9c65d925f3df
service-360 <admin@service360.in>
2026-05-23 21:37:35 +0530

    Fix homepage imports and reusable layout

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client"
 
 import { useState } from "react"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 export default function Home() {
 
